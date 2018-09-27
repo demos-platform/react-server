80acd172f0efbca744312149d7034b2bcbb34661
Muyunyun <328375795@qq.com>
2018-09-27 10:02:42 +0800

    feat: 同构的本质

--- src/server.js
+++ src/server.js
@@ -2,25 +2,31 @@ import React from 'react'
 import ReactDOMServer from 'react-dom/server'
 import express from 'express'
 import App from './app'
+import fetch from 'isomorphic-fetch'
 
 const app = express()
 
+app.use(express.static('dist/public'))
+
 app.get('/', function (req, res) {
-  const domString = ReactDOMServer.renderToString(<App />)
-  res.send(template(domString))
+  const body = ReactDOMServer.renderToString(<App />)
+  // const data = fetch('https://api.github.com/users/gaearon/gists')
+  const result = template(body)
+  res.send(result)
 })
 
-app.listen(3000, () => {
-  console.log('listen in 3000')
+app.listen(8080, () => {
+  console.log('listen in 8080')
 })
 
-const template = (domString) =>
+const template = (body) =>
   `<!DOCTYPE html>
     <html lang="en">
-    <head>
-      <title>Document</title>
-    </head>
-    <body>
-      ${domString}
-    </body>
+      <head>
+        <title>同构实践</title>
+      </head>
+      <body>
+        <div id='root'>${body}</div>
+        <script src='bundle.js'></script>
+      </body>
     </html>`
